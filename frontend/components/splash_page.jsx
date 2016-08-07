3f05128e7f8f1e9b315024f19ba8ec532c241f60
benmcderm <benmcderm@gmail.com>
2016-08-06 22:34:42 -0700

    ES6 updates

--- frontend/components/splash_page.jsx
+++ frontend/components/splash_page.jsx
@@ -21,15 +21,15 @@ const SplashPage = React.createClass({
       <div className="splash-div">
         <h2 className="splash-header">Invest in real estate with others</h2>
         <Slider {...settings} className="slider">
-          <div><Link to={`discover/`} className="splash-link">
+          <div><Link to={'discover/'} className="splash-link">
             View Current Projects
-          </Link><img src="http://res.cloudinary.com/di7w4wcnw/image/upload/v1469815242/slider1_lxh6h3.jpg"></img></div>
-          <div><Link to={`discover/`} className="splash-link">
+          </Link><img role="presentation" src="http://res.cloudinary.com/di7w4wcnw/image/upload/v1469815242/slider1_lxh6h3.jpg"></img></div>
+          <div><Link to={'discover/'} className="splash-link">
             View Current Projects
-          </Link><img src="http://res.cloudinary.com/di7w4wcnw/image/upload/v1469815271/slider2_kfswil.jpg"></img></div>
-          <div><Link to={`discover/`} className="splash-link">
+          </Link><img role="presentation" src="http://res.cloudinary.com/di7w4wcnw/image/upload/v1469815271/slider2_kfswil.jpg"></img></div>
+          <div><Link to={'discover/'} className="splash-link">
             View Current Projects
-          </Link><img src="http://res.cloudinary.com/di7w4wcnw/image/upload/v1469815271/slider3_u8o7zh.jpg"></img></div>
+          </Link><img role="presentation" src="http://res.cloudinary.com/di7w4wcnw/image/upload/v1469815271/slider3_u8o7zh.jpg"></img></div>
         </Slider>
       </div>
     );
